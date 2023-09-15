a00b40ba4cffa23a1dbac9e6b540bc5f3d118405
Semion Rozov <semion.rozov@gmail.com>
2023-09-16 00:02:19 +0200

    signup

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Egg(models.Model):
+    age = models.IntegerField()
+    health = models.FloatField()
+
+
+class Question(models.Model):
+
+    question_text = models.TextField()
+    previous_question = models.OneToOneField(
+        'self', null=True, blank=True, related_name='next_question', on_delete=models.SET_NULL
+    )
+
+class Answer(models.Model):
+
+        ANSWER_CHOICES = (
+            ('A', 'A'),
+            ('B', 'B'),
+            ('C', 'C'),
+            ('D', 'D'),
+        )
+
+        choice = models.CharField(max_length=10, choices=ANSWER_CHOICES)
+        question_text = models.TextField()
+        question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
+from .models import UserProfile
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Create User
+            user_profile = UserProfile.objects.create(
+                user=user,
+                number_of_answered_questions=0,
+            )  # Create UserProfile
+
+            # You can add a success message or redirect the user to a login page here.
+            return redirect('login')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'signup.html', {'form': form})
