4edb667c15070b8a0cfdf47d833f202fa0b52fb9
MikeDoes <michaelam3099@gmail.com>
2023-09-16 15:10:19 +0200

    update on frontend

---
+++ app/static/scripts/fetch_updates.js
@@ -0,0 +1,48 @@
+let currentHealth = 0; // Current health value (starts at 0)
+let currentUsers = 0; // Current users value (starts at 0)
+
+function update(health, users) {
+    // Update active users by counting
+    const active_user_count = document.getElementById("active-users");
+    countFromTo(currentUsers, users, (value) => {
+        active_user_count.innerText = value;
+    });
+    currentUsers = users; // Update the current users value
+
+    // Update health by counting
+    const health_state = document.getElementById('percentage');
+    countFromTo(currentHealth, health, (value) => {
+        health_state.value = value;
+    });
+    currentHealth = health; // Update the current health value
+}
+
+// Function to count from one value to another and update UI
+function countFromTo(start, end, updateUI) {
+    const step = start < end ? 1 : -1; // Determine the direction to count in
+    let currentValue = start;
+
+    function updateValue() {
+        if ((step > 0 && currentValue <= end) || (step < 0 && currentValue >= end)) {
+            updateUI(currentValue);
+            currentValue += step;
+            requestAnimationFrame(updateValue); // Use requestAnimationFrame for smoother transitions
+        }
+    }
+
+    updateValue();
+}
+
+async function fetchHealthData() {
+    try {
+        const response = await fetch('/update');
+        const data = await response.json();
+        
+        update(data.health, data.users);
+    } catch (error) {
+        console.error('Error fetching health data:', error);
+    }
+}
+
+setInterval(fetchHealthData, 1500);
+
