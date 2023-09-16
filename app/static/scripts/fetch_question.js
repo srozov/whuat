812bd52d79c4d5b6023a602a82b25b41c89a9219
MikeDoes <michaelam3099@gmail.com>
2023-09-16 23:58:01 +0200

    fetch question double sending glitch?

--- app/static/scripts/fetch_question.js
+++ app/static/scripts/fetch_question.js
@@ -4,9 +4,9 @@ async function fetchQuestion() {
     try {
         const response = await fetch('/get_random_question');
         const data = await response.json();
-        
+
         document.getElementById('question').innerText = data.question.question_text
-        document.getElementById('choice1').innerText = data.answers.A 
+        document.getElementById('choice1').innerText = data.answers.A
         document.getElementById('choice2').innerText = data.answers.B
         document.getElementById('choice3').innerText = data.answers.C
         document.getElementById('choice4').innerText = data.answers.D
@@ -21,55 +21,50 @@ async function fetchQuestion() {
 fetchQuestion()
 
 
-function sendResponse(question, choice){
-var UPLOAD_URL = "/submit_selected_answer/"; // Your URL endpoint
+function sendResponse(question, choice) {
+    var UPLOAD_URL = "/submit_selected_answer/"; // Your URL endpoint
 
-var data = {
-  question: question,
-  choice: choice
-};
+    var data = {
+        question: question,
+        choice: choice
+    };
 
-var jsonString = JSON.stringify(data);
+    var jsonString = JSON.stringify(data);
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", UPLOAD_URL, true);
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("X-CSRFTOKEN", CSRF_TOKEN);
-xhr.onreadystatechange = function () {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    var response = JSON.parse(xhr.responseText);
-    // Add your logic here
-    setTimeout(() => {
-      location.href = next_url;
-    }, 10);
-  }
-};
-xhr.send(jsonString);
-fetchQuestion()
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", UPLOAD_URL, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-CSRFTOKEN", CSRF_TOKEN);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log('succesfully submited')
+        }
+    };
+    xhr.send(jsonString);
+    fetchQuestion()
 }
 
-document.getElementById('choice1').addEventListener('click', function(){
+document.getElementById('choice1').addEventListener('click', function () {
     sendResponse(
         question = question_id,
         choice = "A"
     )
 })
-document.getElementById('choice2').addEventListener('click', function(){
+document.getElementById('choice2').addEventListener('click', function () {
     sendResponse(
         question = question_id,
         choice = "B"
     )
 })
-document.getElementById('choice3').addEventListener('click', function(){
+document.getElementById('choice3').addEventListener('click', function () {
     sendResponse(
         question = question_id,
         choice = "C"
     )
 })
-document.getElementById('choice4').addEventListener('click', function(){
+document.getElementById('choice4').addEventListener('click', function () {
     sendResponse(
         question = question_id,
         choice = "D"
     )
 })
-
